ab831c69100c65229db673d76561c3de6cd87a9d
Shubhra Mittal <shubhra.goel@gmail.com>
2018-04-16 10:21:47 -0700

    Chapter2

--- M121/Chap2/Lab1.js
+++ M121/Chap2/Lab1.js
@@ -3,15 +3,16 @@ var favorites = [
   "Tom Hanks",
   "Julia Roberts",
   "Kevin Spacey",
-  "George Clooney"];
-
+  "George Clooney"]
+  
 db.movies.aggregate([
-{$match : { $and : [{countries : "USA"}, {"tomatoes.viewer.rating" : {$gte : 3}}]}},
-{$project : {_id: 0,countries:1,cast: 1, title:1, "tomatoes.viewer.rating":1, "title":1,  inter: {$setIntersection : ["$cast", favorites ]}}},
- {$addFields : {   num_favs :{$size : {$ifNull : ["$inter", []]}} }},
-{$sort : {num_favs :-1, "tomatoes.viewer.rating" : -1, title:-1 }},
-{$skip : 24}
+{$match : {$and : [{"tomatoes.viewer.rating" : {$gte:3}},{countries : "USA"}]}},
+{$project : {title:1, rating : "$tomatoes.viewer.rating", favActors : {$setIntersection : ["$cast", favorites]}}},
+{$addFields: {num_favs : {$size : {$ifNull : ["$favActors", []]}}}},
+{$sort : {num_favs: -1, rating:-1, title:-1}},
+{$skip: 24}
+
+
+
+
 ])
-
-
-//db.movies.find({}).limit(1).pretty()
--- M121/Chap2/Lab2.js
+++ M121/Chap2/Lab2.js
@@ -1,36 +1,14 @@
-db.movies.aggregate([
+var x_max = 1521105;
+var x_min = 5;
+var min = 1;
+var max = 10;
 
-{$match : {$and : [
-  {languages : {$exists :1}},
-  {languages : "English"},
-  {"imdb.rating"  : {$gte :1}}, 
-  {"imdb.votes": {$gte :1}},
-  {year : {$gte : 1990}} //Don't use {released : {$gt : ISODate("1989-12-31")}}  as some documents dont have released field
-  ]}}
-, 
-{$addFields : {scaledVotes : 
- {
-    $add: [
-      1,
-      {
-        $multiply: [
-          9,
-          {
-            $divide: [
-              { $subtract: ["$imdb.votes", 5] },
-              { $subtract: [1521105, 5] }
-            ]
-          }
-        ]
-      }
-    ]
-  }
-
-
-}},
-
-{$addFields : {normalized_rating : {$divide : [ {$add : ["$imdb.rating", "$scaledVotes"]}  , 2]}}},
-{$sort : {normalized_rating : 1}}
   
-])
+db.movies.aggregate([
+{$match : {$and : [ {languages: "English"}, {"imdb.rating" : {$gte:1}}, {"imdb.votes" : {$gte :1}}, {year : {$gte: 1990}}]}},
+{$project : {title:1 , imdb_votes: "$imdb.votes",imdb_rating: "$imdb.rating"  ,_id:0}},
+{$addFields: {scaled_votes : { $add: [1, {$multiply : [9, {$divide : [{$subtract : ["$imdb_votes", x_min]}, {$subtract : [x_max, x_min]}]}]}]}}},
+{$addFields : {normalized_rating : {$avg : ["$scaled_votes","$imdb_rating" ]}}},
+{$sort : {normalized_rating : 1}}
 
+])
